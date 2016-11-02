b89232890906f67bc6fc9adcb86e84d880f3ed8b
djayfresh <djfresh1@comcast.net>
2016-11-02 09:02:46 -0500

    Inital setup for router service

---
+++ AngularSite/app/services/router.service.ts
@@ -0,0 +1,32 @@
+﻿import { Injectable } from '@angular/core';
+import { Router as AngularRouter, ActivatedRoute, RouterState } from '@angular/router';
+
+@Injectable()
+export class RouterService {
+    
+    subscribersToParams: Subscriber[] = [];
+
+    constructor(private router: AngularRouter){
+        this.init();
+    }
+
+    private init() {
+        this.router.routerState.root.params.subscribe((params) => {
+            console.log("Params", params);
+            if(this.subscribersToParams) {
+                this.subscribersToParams.forEach((subsriber) => {
+                    if(subsriber && subsriber.callback){
+                        subsriber.callback(params);
+                    }
+                });
+            }
+        
+        });
+
+    }
+}
+
+export interface Subscriber {
+    id: number;
+    callback: Function;
+}
